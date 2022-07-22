e78f250c2d10c527e64444f7ad62c8a8d5fdb264
Timothy Henry <tim87henry@gmail.com>
2022-07-22 20:29:38 +1000

    Added user sign up with validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,44 @@
 var User = require('../models/user');
+const {body, validationResult, check} = require('express-validator');
 
 exports.user_create_get = function(req, res, next) {
     res.render('sign_up');
 }
 
-exports.user_create_post = function(req, res, next) {
-    res.send("Create a user post")
-}
+exports.user_create_post = [
+    body('first_name', 'Please enter first name').trim().isLength({ min: 1}).escape(),
+    body('last_name', 'Please enter last name').trim().isLength({ min: 1}).escape(),
+    body('username', 'Please enter username').trim().isLength({ min: 1}).escape(),
+    body('password', 'Please enter password').trim().isLength({ min: 1}).escape(),
+    check(
+        'custom_password',
+        'Password confirmation field must have the same value as the password field',
+    )
+    .exists()
+    .custom((value, { req }) => value === req.body.password),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render('sign_up', {errors: errors.array()});
+        } else {
+            var user = new User(
+                {
+                    first_name: req.body.first_name,
+                    last_name: req.body.last_name,
+                    username: req.body.username,
+                    password: req.body.password,
+                    country: req.body.country,
+                    is_member: false,
+                    is_admin: false
+                }
+            );
+    
+            user.save(function(err) {
+                if (err) { return next(err)}
+                res.redirect('/')
+            })
+        }
+}]
 
 exports.user_join_get = function(req, res, next) {
     res.send("User becomes a member get")
