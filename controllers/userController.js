8e9079b3931063c78de09d40aa269d9b5582a4f3
Timothy Henry <tim87henry@gmail.com>
2022-08-07 23:18:10 +1000

    Now user is logged in automatically after signing up. Cannot create 2 users with same username. TODO: Fix a bug in validating user sign up

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,6 +42,18 @@ exports.user_create_post = [
     body('first_name', 'Please enter first name').trim().isLength({ min: 1}).escape(),
     body('last_name', 'Please enter last name').trim().isLength({ min: 1}).escape(),
     body('username', 'Please enter username').trim().isLength({ min: 1}).escape(),
+    body('username', 'Username already taken').custom((value, { req }) => {
+      User.findOne({ username: value }, (err, user) => {
+      if (err) { 
+        throw new Error(err);
+      }
+      if (user) {
+        throw new Error('Username already exists');
+      } else {
+        return true;
+      }
+      })
+    }),
     body('password', 'Please enter password').trim().isLength({ min: 1}).escape(),
     check('password').exists(),
     check(
@@ -73,8 +85,11 @@ exports.user_create_post = [
         
                     user.save(function(err) {
                         if (err) { return next(err)}
-                        res.redirect('/')
-                    })
+                        req.login(user, function(err1) {
+                          if (err) { return next(err1); }
+                          res.redirect('/');
+                        });
+                    });
                 }
             });
         }
